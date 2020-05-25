1c3bccb399c6e09d1bd7ebb8ec762d0fcfab27a7
x0xO <git@masolit.me>
2020-05-25 18:34:49 +0200

    fmt in hhttp/examples/session.go

--- examples/session.go
+++ examples/session.go
@@ -10,7 +10,7 @@ func main() {
 	URL := "https://httpbingo.org/cookies"
 
 	// example 1
-    // chains session
+	// chains session
 	r, _ := hhttp.NewClient().Get(URL + "/set?name1=value1&name2=value2").Do()
 	r, _ = r.Session.Get(URL).Do()
 	fmt.Println(r.Body) // check if cookies in response {"name1":"value1","name2":"value2"}
